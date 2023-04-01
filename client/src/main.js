0e152f363d54c312dec949738b1df1a48f3075ff
gimleng <e0426020@u.nus.edu>
2023-04-01 15:15:38 +0800

    Add basic routing between vue and flask

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import App from './App.vue';
+import Ping from './components/Ping.vue';
 
-createApp(App).mount('#app')
+const routes = [
+    {
+        path: '/ping',
+        name: 'Ping',
+        component: Ping,
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+createApp(App)
+.use(router)
+.mount('#app');
