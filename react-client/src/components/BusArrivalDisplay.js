2b584043d6767de2b40eb88ede62fa72eb698010
gimleng <e0426020@u.nus.edu>
2023-06-28 12:10:04 +0800

    add inconsistent flag

--- react-client/src/components/BusArrivalDisplay.js
+++ react-client/src/components/BusArrivalDisplay.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import ArrivalListView from './ArrivalListView'
 import ArrivalTableView from './ArrivalTableView'
+import DisclaimerMessage from './DisclaimerMessage'
 
 function BusArrivalDisplay({ arrivalData, updateTime, refreshData, selectedStop, stops, busDiff, isLoading }) {
     const [listView, setListView] = useState(true)
@@ -21,11 +22,30 @@ function BusArrivalDisplay({ arrivalData, updateTime, refreshData, selectedStop,
           }, 1000)
     }
 
+    function isConsistent(arrivalData, busDiff) {
+        for (const timeDiff of Object.values(busDiff)) {
+            if (Math.max(...timeDiff) - Math.min(...timeDiff) > 240) {
+                return false
+            }
+        }
+        for (const bus of Object.values(arrivalData)) {
+            // Check if all non forecasted timings have the same bus type and special departure status
+            const actualTimings = Object.values(bus['busTimings']).filter(t => !t['isForecasted'])
+            const busType = new Set(actualTimings.map(t => t['busType']))
+            const isSpecial = new Set(actualTimings.map(t => t['isSpecial']))
+            if (busType.size > 1 || isSpecial.size > 1) {
+                return false
+            }
+        }
+        return true
+    }
+
     return (
         <div className='row justify-content-center'>
             <hr />
             <div className='col-auto text-center mb-1'>
                 <h5>Arrivals for <u>{ stops.filter(s => s['stopSequence'] === selectedStop)[0]['name'] }</u></h5>
+                {!isConsistent(arrivalData, busDiff) && <DisclaimerMessage message='Forecast may be inconsistent and refresh is recommended.' />}
                 {listView 
                     ? <ArrivalListView arrivalData={arrivalData} updateTime={updateTime} selectedStop={selectedStop} busDiff={busDiff} /> 
                     : <ArrivalTableView arrivalData={arrivalData} updateTime={updateTime} selectedStop={selectedStop} stops={stops} /> }
